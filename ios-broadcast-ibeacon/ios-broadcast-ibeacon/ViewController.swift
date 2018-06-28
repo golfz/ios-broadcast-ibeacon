bff4543931418a8434a6eb754c2e3338d3493531
Surattikorn Chumkaew <surattikorn@gmail.com>
2018-06-28 07:50:33 +0700

    start broadcast when CBPeripheralManager.state is .poweredon

--- ios-broadcast-ibeacon/ios-broadcast-ibeacon/ViewController.swift
+++ ios-broadcast-ibeacon/ios-broadcast-ibeacon/ViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+    
+    var region: CLBeaconRegion?
+    var peripheralData: NSDictionary?
+    var peripheralManager: CBPeripheralManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.region = createBeaconRegion()
+        (self.peripheralManager, self.peripheralData) = createPeripheralDevice(region: region!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createBeaconRegion() -> CLBeaconRegion? {
+        let proximityUUID = UUID(uuidString:
+            "00112233-4455-6677-8899-AABBCCDDEEFF")
+        let major : CLBeaconMajorValue = 123
+        let minor : CLBeaconMinorValue = 456
+        let beaconID = "com.example.ios-broadcast-ibeacon"
+        
+        return CLBeaconRegion(proximityUUID: proximityUUID!, major: major,
+                              minor: minor, identifier: beaconID)
+    }
+    
+    func createPeripheralDevice(region : CLBeaconRegion) -> (CBPeripheralManager?, NSDictionary?) {
+        let peripheral = CBPeripheralManager(delegate: self, queue: nil)
+        let peripheralData = region.peripheralData(withMeasuredPower: nil)
+        return (peripheral, peripheralData)
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .poweredOn:
+            print(".poweredOn")
+            peripheral.startAdvertising(((peripheralData as NSDictionary!) as! [String : Any]))
+        case .poweredOff:
+            print(".poweredOff")
+            peripheral.stopAdvertising()
+        default:
+            print("default")
+            peripheral.stopAdvertising()
+        }
     }
 
 
